7bfb0982210d58ede170bcee85cb671fa014ba4b
suriya3153 <107272107+suriya3153@users.noreply.github.com>
2022-10-25 22:29:30 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ st.header("baby")
 a=st.text_input("type yes")
 if len(a)!=0:
     if a=="yes":
-        st.write("love you abi"*1000)
+        st.write("love you abi ❤"*1000)
     else:
         st.write("enter right input yes or no")
         
