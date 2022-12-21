7e5ea05b518753b41c27a259e366e80069c6f8b5
suriya3153 <107272107+suriya3153@users.noreply.github.com>
2022-12-21 17:36:38 +0530

    Delete myapp.py

--- myapp.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.header("baby")
-a=st.text_input("type yes")
-if len(a)!=0:
-    if a=="yes":
-        st.write("love you abi ❤"*1000)
-    else:
-        st.write("enter right input yes or no")
-        
